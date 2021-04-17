d5d11e0bd09a7c304107639b38e48673ad9f5339
gebrielmbelaineh <gebriel.belaineh.ug@dartmouth.edu>
2021-04-16 22:57:46 -0400

    Done withSA2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("Yay!")
+import $ from 'jquery';
+import './style.scss';
+
+let num = 0;
+setInterval(() => {
+  num += 1;
+  $('#main').html(`You have been on this page for ${num} seconds`);
+}, 1000);
